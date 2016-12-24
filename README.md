4ba6465acc5fe4e67b837374ec271279a406494d
Mojtaba Dashtinejad <mojtaba@gmail.com>
2016-12-24 11:26:36 +0330

    initialize gulpfile

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # regate
 Regate · React components for building form inputs.
+
+
+1. Install dependencies:
+
+```
+$ npm install
+```
+
+2. Start the project:
+
+```
+$ npm start
+```
+
+
+# Test
+
+Inside the `test` folder, run:
+
+```
+$ npm run test -- --watch
+```
+
+# Docs
+
+See `site/README.md`
+
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,22 +1,30 @@
 import gulp from 'gulp'
 import babel from 'gulp-babel'
 import webpack from 'webpack-stream'
-import browserSync from 'browser-sync'
-import webpackConfig from './webpack.config.babel'
+import webpackConfig from './webpack.config.babel';
 
 const paths = {
   allSrcJs: 'src/**/*.js?(x)',
+  clientEntryPoint: 'src/index.js',
   distDir: './distrubution'
 }
 
-gulp.task('build', () =>
+gulp.task('build', ['build:commonjs', 'build:umd'])
+
+gulp.task('build:commonjs', () =>
   gulp.src(paths.allSrcJs)
     .pipe(babel())
     .pipe(gulp.dest(paths.distDir))
 )
 
-gulp.task('watch', () => 
+gulp.task('build:umd', () =>
+  gulp.src(paths.clientEntryPoint)
+    .pipe(webpack(webpackConfig))
+    .pipe(gulp.dest(paths.distDir))
+)
+
+gulp.task('build:watcher', () => 
   gulp.watch(paths.allSrcJs, ['build'])
 )
 
-gulp.task('default', ['build', 'watch'])
+gulp.task('default', ['build', 'build:watcher'])
